e6c54d3b96731406eeffce358f8ac57128f436e8
Dany <dani_jdvlive.com.ar>
2021-05-07 22:07:48 -0300

    Realizado (hay que mejorarlo)

--- src/main/kotlin/ar/edu/unahur/obj2/socios/Cliente.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/socios/Cliente.kt
@@ -1,4 +1,51 @@
 package ar.edu.unahur.obj2.socios
 
-class Cliente {
+import kotlin.math.max
+
+abstract class Estado {
+    abstract fun propina(unImporte: Int, unCliente: Cliente) : Int
+
+}
+
+object enojado: Estado() {
+    override fun propina(unImporte : Int, unCliente: Cliente) = 0
+}
+
+object feliz: Estado() {
+    override fun propina(unImporte : Int, unCliente: Cliente) = (unImporte / 100) * 25
+}
+
+object indiferente: Estado() {
+    override fun propina(unImporte : Int, unCliente: Cliente): Int = unCliente.bolsillo
+}
+
+object resfriado: Estado() {
+    override fun propina(unImporte: Int, unCliente: Cliente) = unImporte
+}
+
+/**************Barrio**************/
+
+abstract class Barrio(){
+   abstract fun calcularPropina(unaPropina: Int) : Int
+}
+
+object LasRosas : Barrio(){
+    override fun calcularPropina(unaPropina: Int) = unaPropina + 50
+}
+object LasLauchas : Barrio(){
+    override fun calcularPropina(unaPropina: Int) = unaPropina / 2
+}
+object BarrioVerde : Barrio(){
+    override fun calcularPropina(unaPropina: Int) = max(unaPropina, 200)
+}
+object LasTorres : Barrio() {
+    override fun calcularPropina(unaPropina: Int) = unaPropina
+}
+
+class Cliente(val estadoDeAnimo: Estado, var bolsillo: Int, var barrio : Barrio) {
+
+    fun propina(unImporte: Int) = estadoDeAnimo.propina(unImporte, this)
+
+    fun cuantaPropinaDejaria(unImporte: Int) = barrio.calcularPropina(this.propina(unImporte))
+
 }
